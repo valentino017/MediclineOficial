499a8d141e49c212fdb166d45fed2ad53dea9fb7
valentino17-0 <82229577+valentino17-0@users.noreply.github.com>
2023-05-12 23:57:05 -0500

    primer commit

---
+++ src/main/java/com/pe/spring/MediclineOficialApplication.java
@@ -0,0 +1,13 @@
+package com.pe.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediclineOficialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MediclineOficialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pe/spring/MediclineOficialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pe.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediclineOficialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
